67573f6fa13869eb79927592f4df1f47be44a580
Bianca Duarte <bianca.du.raposo@gmail.com>
2021-11-30 13:02:40 -0300

    Funcionalidades de validação do formulário, preenchimento dos campos e  senhas compativeis

--- projetocrud/dist/init.js
+++ projetocrud/dist/init.js
@@ -4,6 +4,13 @@ const username = document.querySelector('.username');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 const password2 = document.querySelector('.password2');
+// definindo o evento do form
+form.addEventListener('submit', function (event) {
+    //eu quero que eele cancele o evento de submeter para ir para uma outra página
+    event.preventDefault();
+    excluirMSGErro(form); // exclui a mensagem de erro com o campo alterado
+    verificarCamposVazios(username, email, password, password2);
+});
 //Função que vai verificar se os campos estão vazios
 function verificarCamposVazios(...inputs) {
     inputs.forEach((campo) => {
@@ -13,15 +20,38 @@ function verificarCamposVazios(...inputs) {
         }
     });
 }
-// definindo o evento do form
-form.addEventListener('submit', function (event) {
-    //eu quero que eele cancele o evento de submeter para ir para uma outra página
-    event.preventDefault();
-    verificarCamposVazios(username, email, password, password2);
-});
+//Excluir a mensagem de error
+function excluirMSGErro(form) {
+    form.querySelectorAll('.show-error-message').forEach(function (item) {
+        item.classList.remove('show-error-message');
+    });
+}
 function apresentaMSGErro(input, msg) {
     let formField = input.parentElement;
     let errorMessage = formField.querySelector('.error-message');
     errorMessage.innerText = msg;
     formField.classList.add('show-error-message');
+}
+form.querySelectorAll('input').forEach(elemento => {
+    elemento.addEventListener('blur', (event) => {
+        event.preventDefault();
+        if (!elemento.value) {
+            apresentaMSGErro(elemento, 'O camponão pode ser vazio!');
+        }
+        else {
+            let formField = elemento.parentElement;
+            formField.classList.remove('show-error-message');
+            if (elemento.className == 'password', elemento.className == 'password2') {
+                verificarSenha(password, password2);
+            }
+        }
+    });
+});
+function verificarSenha(password, password2) {
+    if (password.value.length < 8) {
+        apresentaMSGErro(password, "A senha dever ter no mínimo 8 caracteres.");
+    }
+    if (password.value != password2.value) {
+        apresentaMSGErro(password2, "As senhas devem ser iguais.");
+    }
 }
--- projetocrud/src/init.ts
+++ projetocrud/src/init.ts
@@ -4,6 +4,14 @@ const email =document.querySelector('.email')as HTMLInputElement;
 const password =document.querySelector('.password')as HTMLInputElement;
 const password2 =document.querySelector('.password2')as HTMLInputElement;
 
+// definindo o evento do form
+form.addEventListener('submit', function(event: Event){
+    //eu quero que eele cancele o evento de submeter para ir para uma outra página
+    event.preventDefault();
+    excluirMSGErro(form); // exclui a mensagem de erro com o campo alterado
+    verificarCamposVazios(username, email, password, password2);
+});
+
 //Função que vai verificar se os campos estão vazios
 function verificarCamposVazios(...inputs:HTMLInputElement[]):void{
     inputs.forEach((campo)=>{
@@ -14,12 +22,12 @@ function verificarCamposVazios(...inputs:HTMLInputElement[]):void{
     })
 }
 
-// definindo o evento do form
-form.addEventListener('submit', function(event: Event){
-    //eu quero que eele cancele o evento de submeter para ir para uma outra página
-    event.preventDefault();
-    verificarCamposVazios(username, email, password, password2);
-});
+//Excluir a mensagem de error
+function excluirMSGErro(form: HTMLFormElement):void{
+    form.querySelectorAll('.show-error-message').forEach(function(item){
+        item.classList.remove('show-error-message');
+    })
+}
 
 function apresentaMSGErro(input:HTMLInputElement, msg:string){
     let formField= input.parentElement as HTMLDivElement;
@@ -28,3 +36,27 @@ function apresentaMSGErro(input:HTMLInputElement, msg:string){
     formField.classList.add('show-error-message'); 
 }
     
+form.querySelectorAll('input').forEach(elemento=>{
+    elemento.addEventListener('blur', (event)=>{
+        event.preventDefault();
+        if(!elemento.value){
+            apresentaMSGErro(elemento,'O camponão pode ser vazio!');
+        }else{
+            let formField = elemento.parentElement as HTMLDivElement;
+            formField.classList.remove('show-error-message');
+            if (elemento.className == 'password', elemento.className == 'password2'){
+            verificarSenha(password, password2);
+            }
+        }
+    })
+})
+
+function verificarSenha(password:HTMLInputElement, password2:HTMLInputElement):void{
+    if(password.value.length <8){
+        apresentaMSGErro(password, "A senha dever ter no mínimo 8 caracteres.")
+    }
+    
+    if (password.value != password2.value){
+        apresentaMSGErro(password2, "As senhas devem ser iguais.")
+    }
+}
