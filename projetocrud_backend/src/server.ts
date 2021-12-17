11cb344b1cac24ee3aaef6e511660cc7129e0538
Bianca Duarte <bianca.du.raposo@gmail.com>
2021-12-17 14:04:13 -0300

    Configurando Docker

--- projetocrud_backend/src/server.ts
+++ projetocrud_backend/src/server.ts
@@ -14,7 +14,7 @@ connection.schema
     table.string("password");
     table.timestamps();
   })
-  .then(() => console.log("Tabela criada"))
+  .then(() => console.log("Tabela de usuários criada com sucesso"))
   .catch((erro) => console.log(erro));
 
 const app = express();
@@ -23,37 +23,40 @@ app.use(express.json());
 
 const port = process.env.PORT;
 
-//leitura de usuario
+//leitura dos usuarios
 app.get("/usuarios", (req, res) => {
-  let usuarios = connection
+  connection
     .select()
     .table("usuarios")
     .then((usuarios) => {
+      console.log(usuarios);
       res.json({
         status: "ok",
         mensagem: "requisição com GET",
         usuarios: usuarios,
       });
-    });
+    })
+    .catch((erro) => console.log(erro));
 });
 
+//criação de usuários
 app.post("/usuarios", (req, res) => {
-  let usuarios = req.body;
-  usuarios.created_at = new Date();
+  let usuario = req.body;
+  usuario.created_at = new Date();
 
   connection("usuarios")
-    .where("email", usuarios.email)
+    .where("email", usuario.email)
     .then((usuarios) => {
       if (usuarios.length > 0) {
         res.json({
           status: "erro",
-          mensagem: "Email já cadastrado",
+          mensagem: "E-mail já cadastrado na base de dados",
         });
       } else {
         connection("usuarios")
-          .insert(usuarios)
+          .insert(usuario)
           .then((dados) => {
-            console.log("Usuário inserido com sucesso");
+            console.log("Usuário inserido com sucesso!");
             res.json({
               status: "ok",
               mensagem: "Usuário inserido com sucesso",
@@ -63,24 +66,41 @@ app.post("/usuarios", (req, res) => {
           .catch((erro) => {
             res.json({
               status: "erro",
-              mensagem: "Erro ao cadastrar!",
+              mensagem: "Erro ao inserir usuário",
               erro: erro,
             });
           });
       }
     })
-    .catch((erro) => {
-      res.json({
-        status: "erro",
-        mensagem: "Erro ao inserir o usuário!",
-        erro: erro,
-      });
-    });
+    .catch((erro) => console.log(erro));
+});
+
+app.patch("/usuarios/:id", (req, res) => {
+  let id = req.params.id;
+  let usuario = req.body;
+  usuario.updated_at = new Date();
+
+  connection("usuarios")
+    .where("id", id)
+    .update(usuario)
+    .then((resultado) => {
+      if (resultado == 1) {
+        res.json({
+          status: "ok",
+          mensagem: "Usuário atualizado com sucesso!",
+        });
+      } else {
+        res.json({
+          status: "warning",
+          mensagem: "Nenhum registro atualizado",
+        });
+      }
+    })
+    .catch((erro) => console.log(erro));
 });
 
 app.delete("/usuarios/:id", (req, res) => {
   const id = req.params.id;
-
   connection("usuarios")
     .where("id", id)
     .delete()
@@ -105,28 +125,4 @@ app.delete("/usuarios/:id", (req, res) => {
     });
 });
 
-app.patch("/usuarios/:id", (req, res) => {
-  let id = req.params.id;
-  let usuario = req.body;
-  usuario.updated_at = new Date();
-
-  connection("usuarios")
-    .where("id", id)
-    .update(usuario)
-    .then((resultado) => {
-      if (resultado == 1) {
-        res.json({
-          status: "ok",
-          mensagem: "Dados atualizados com sucesso!",
-        });
-      } else {
-        res.json({
-          status: "warning",
-          mensagem: "Nenhum registro atualizado...",
-        });
-      }
-    })
-    .catch((erro) => console.log(erro));
-});
-
-app.listen(port, () => console.log("Servidor rodando"));
+app.listen(port, () => console.log("Servidor rodando normalmente ..."));
